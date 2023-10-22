3b2cfbe83c64d144c13bdabdf4af7d5de518e914
Shakil53 <shakilahmed.dcc@gmail.com>
2023-10-22 12:59:22 +0600

    footer and content structered

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,27 @@ const Home = () => {
     return (
         <div>
             <NavBar></NavBar>
+
+
+            <section>
+
+                <div className="folder-str-content"></div>
+                <h1 style={{ margin: 'auto', width: '50%' }}> Folder Structer Task (CRUD) Operation.</h1>
+
+                <div className="main-content">
+                    <h3>Root Folder</h3>
+                </div>
+
+
+
+
+
+
+
+
+            </section>
+
+
             <Footer></Footer>
 
         </div>
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,34 @@
+import './Footer.css';
+import logo from '../../../assets/images/headless-logo.png'
+import { Link } from 'react-router-dom';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { FaMailBulk, FaFacebookF, FaInstagram } from 'react-icons/fa';
 
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer Section</h3>
-        </div>
+        <footer className='footer-wraper'>
+            <div className='footer'>
+                <img src={logo}></img>
+            </div>
+            {/* footer contain */}
+            <div className='footer-details'>
+                <p><FaTelegramPlane></FaTelegramPlane><span style={{ margin: "2px" }}>Dhaka +(880) 183 224-0871</span>
+                    <span style={{ margin: '5px' }}><FaMailBulk></FaMailBulk> info@headless.ltd</span></p>
+                <p>68/8-B, Zigatola PO, Dhaka-1209</p>
+                <p>copyright@<span style={{ margin: "5px" }}><Link to='/'>Headless Technology ltd</Link></span></p>
+
+            </div>
+            {/* social icons */}
+            <div className='footer-social-icon'>
+                <p>Follow us: </p>
+                <div className='icon'><Link to={'https://www.instagram.com/shakil_ahmed_aries/'}><FaInstagram></FaInstagram></Link></div>
+                <div className='icon'> <Link to={'https://www.facebook.com/shakilahmedaries01/'}><FaFacebookF ></FaFacebookF></Link> </div>
+                <div className='icon'><Link to={'https://www.instagram.com/shakil_ahmed_aries/'}><FaInstagram></FaInstagram></Link></div>
+
+            </div>
+
+        </footer >
     );
 };
 
