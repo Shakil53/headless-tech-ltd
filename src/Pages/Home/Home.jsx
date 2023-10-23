9858e213a58ff412d79461a316ff01152956a43d
Shakil53 <shakilahmed.dcc@gmail.com>
2023-10-23 12:48:59 +0600

    button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,34 +1,50 @@
 import Footer from "../Shared/Footer/Footer";
 import NavBar from "../Shared/NavBar/NavBar";
+import { FaAngleRight } from 'react-icons/fa';
+import './Home.css';
 
 const Home = () => {
+
+
+    const handleArrow = (event) => {
+        event.preventDefault()
+
+    }
+    const handleAddFolder = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <div>
             <NavBar></NavBar>
 
 
-            <section>
+            <section className="content-wraper">
 
                 <div className="folder-str-content"></div>
                 <h1 style={{ margin: 'auto', width: '50%' }}> Folder Structer Task (CRUD) Operation.</h1>
 
-                <div className="main-content">
-                    <h3>Root Folder</h3>
+                <div onClick={handleArrow} className="mainContent">
+                    <h3
+                        style={{
+                            marginLeft: '100px'
+                        }}
+
+                    ><FaAngleRight></FaAngleRight>Root Folder
+                        <button onClick={handleAddFolder} className="btn">Add Folder</button>
+                        <button className="btn">Delete Folder</button>
+                    </h3>
+
                 </div>
 
 
 
-
-
-
-
-
-            </section>
+            </section >
 
 
             <Footer></Footer>
 
-        </div>
+        </div >
     );
 };
 
